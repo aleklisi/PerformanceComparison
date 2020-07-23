57ee898bc02da5e7f849e69990179339f1c1334f
Aleksander Lisiecki <sebaczechu123@gmail.com>
2020-07-23 18:24:57 +0200

    Add Wilcoxon test

---
+++ wilcoxon_test.R
@@ -0,0 +1,7 @@
+# Wilcoxon Test
+
+wilcox.test(time ~ test, data = first_and_second_erlang_runs, alternative = "two.sided")
+
+# The p-value of the test is more than the significance level alpha = 0.05.
+# We can conclude that first run median weight is not significantly different from the second run median time.
+
